5ce338379842aa39a78d6ce7c009c4340e0494f9
太田　一毅 <kazukiota121@gmail.com>
2015-04-12 13:54:43 +0900

    countのswift版FirstCommit!

--- Count/ViewController.swift
+++ Count/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var label:UILabel!
+    var number:Int = 0;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    @IBAction func plus(){
+        number++;
+        label.text = "\(number)"
+    }
 }
 
